85ef5a47f5840183921bcc4237fa6d6cfad44b46
elliottfeltham <elliottfeltham98@gmail.com>
2025-02-22 23:08:30 +0000

    Add remove buttons per element and format javascript code

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ const addButton = document.querySelector("#addButton");
 const clearButton = document.querySelector("#clearButton");
 const bookDialog = document.querySelector("#book-form-dialog");
 const form = document.querySelector(".book-form");
-const formAddButton = bookDialog.querySelector("#add-form-btn");
+const formAddButton = document.querySelector("#add-form-btn");
 const formCloseButton = document.querySelector("#close-form-btn");
 
+// Create an array to add too
 const myLibrary = [];
 
+// Functions //
+
+// Book Constructor
 function Book(title, author, pages, read) {
 	this.title = title;
 	this.author = author;
@@ -18,50 +22,69 @@ function Book(title, author, pages, read) {
 	};
 }
 
+// Add books to library
 function addBookToLibrary(title, author, pages, read) {
 	const book = new Book(title, author, pages, read);
 
 	myLibrary.push(book);
 }
 
-function clearLibrary() {
-	myLibrary.length = 0;
-	libraryContainer.textContent = "";
-}
-
+// Display books on the page
 function displayBooks() {
 	libraryContainer.innerHTML = "";
 
-	for (let book of myLibrary) {
+	myLibrary.forEach((book, index) => {
 		const card = document.createElement("div");
 		card.classList.add("book-card");
 
+		// This index number is used to delete the specific book
+		card.dataset.indexNumber = index;
+
 		const title = document.createElement("h3");
 		title.classList.add("title");
+		title.textContent = `${book.title}`;
 
 		const author = document.createElement("span");
 		author.classList.add("author");
+		author.textContent = `By: ${book.author}`;
 
 		const pages = document.createElement("span");
 		pages.classList.add("pages");
+		pages.textContent = `Pages: ${book.pages}`;
 
 		const read = document.createElement("span");
 		read.classList.add("read");
-
-		title.textContent = `${book.title}`;
-		author.textContent = `By: ${book.author}`;
-		pages.textContent = `Pages: ${book.pages}`;
 		read.textContent = `Status: ${book.read}`;
 
-		card.append(title, author, pages, read);
+		const removeBookButton = document.createElement("button");
+		removeBookButton.classList.add("remove-book-button");
+		removeBookButton.textContent = "Remove";
+
+		card.append(title, author, pages, read, removeBookButton);
 		libraryContainer.append(card);
-	}
+	});
 }
 
-addButton.addEventListener("click", function () {
+// Clear library
+function clearLibrary() {
+	myLibrary.length = 0;
+	libraryContainer.innerHTML = "";
+}
+
+// Close form
+function closeForm() {
+	form.reset();
+	bookDialog.close();
+}
+
+// Event Listeners //
+
+// Open form pop up
+addButton.addEventListener("click", () => {
 	bookDialog.showModal();
 });
 
+// Use form data from user to add books to library
 formAddButton.addEventListener("click", (event) => {
 	event.preventDefault();
 
@@ -75,14 +98,23 @@ formAddButton.addEventListener("click", (event) => {
 	addBookToLibrary(title, author, pages, read);
 
 	displayBooks();
-	form.reset();
-	bookDialog.close();
+	closeForm();
 });
 
+// Close form pop ups
 formCloseButton.addEventListener("click", (event) => {
 	event.preventDefault();
-	form.reset();
-	bookDialog.close();
+	closeForm();
 });
 
+// Check for remove button clicks and delete based on elements index number
+libraryContainer.addEventListener("click", (event) => {
+	if (event.target.classList.contains("remove-book-button")) {
+		const index = event.target.parentElement.dataset.indexNumber;
+		myLibrary.splice(index, 1);
+		displayBooks();
+	}
+});
+
+// Clear library array
 clearButton.addEventListener("click", clearLibrary);
